a9219060eb0754b229b9da7457beb77e258179c1
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-12 01:11:55 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,2 +1,20 @@
 class CouponsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to coupons_path(@coupon)
+  end
 end
