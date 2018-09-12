f791f67fbcef99897c7c4efead8e0129d6a42a30
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-12 01:15:47 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/create' => 'coupons#create'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'coupons#index'
+  #root 'coupons#index'
 
 
   # Example of regular route:
